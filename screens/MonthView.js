eeb7f38ee9d0d007cdbb53d76e1bc928ead7c57b
Justin Siu <justsiu545@gmail.com>
2023-01-05 20:10:18 -0800

    testing

--- screens/MonthView.js
+++ screens/MonthView.js
@@ -45,6 +45,7 @@ export default function MonthView({ navigation }) {
             <CalendarContent 
                 viewDay = {viewDay}
             />
+            {/* test */}
         </View>
     );
 }
